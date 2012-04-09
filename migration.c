8798b68da03479beb96b2a56e0798a26d0f17561
sx <sx@debian-q9300.(none)>
2012-04-09 13:46:16 +0800

    remove nanosleep

--- migration.c
+++ migration.c
@@ -85,7 +85,6 @@ int do_migrate(Monitor *mon, const QDict *qdict, QObject **ret_data)
     int inc = qdict_get_try_bool(qdict, "inc", 0);
     const char *uri = qdict_get_str(qdict, "uri");
     const char *config_file = "/root/images/config";//qdict_get_str(qdict, "config");
-    struct timespec slave_sleep = {30, 0};
 
     if (current_migration &&
         current_migration->get_status(current_migration) == MIG_STATE_ACTIVE) {
@@ -97,8 +96,6 @@ int do_migrate(Monitor *mon, const QDict *qdict, QObject **ret_data)
         return -1;
     }
 
-    nanosleep(&slave_sleep, NULL);
-    DPRINTF("Config file is %s\n", config_file);
     if (strstart(uri, "tcp:", &p)) {
         //classicsong
         s = tcp_start_outgoing_migration(mon, p, max_throttle, detach,
