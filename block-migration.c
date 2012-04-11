496b58fd469d2b9a070c0224d0366ccbc7e52877
root <root@debian.localdomain>
2012-04-11 14:17:01 -0400

    no qemu_aio_flush()

--- block-migration.c
+++ block-migration.c
@@ -887,7 +887,7 @@ disk_save_master(Monitor *mon, struct migration_task_queue *task_q, QEMUFile *f)
             }
 
             if (flush_batch == DEFAULT_DISK_BATCH_LEN) {
-                qemu_aio_flush();
+                //qemu_aio_flush();
                 //data_sent += flush_blks_master(task_q, f, 0);
                 data_sent += mig_save_device_iter_sync(mon, f, 0, task_q);
                 flush_batch = 0;
