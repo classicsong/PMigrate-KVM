cdcf3d52d9e1057596d2db62e3558fb70416d601
sx <sx@debian-q9300.(none)>
2012-04-20 13:13:05 +0800

    remove signal for slaves

--- migration-slave.c
+++ migration-slave.c
@@ -123,15 +123,8 @@ start_host_slave(void *data) {
     int i, ret;
     QEMUFile *f;
     struct timespec slave_sleep = {0, 100000};
-    sigset_t set;
     unsigned long data_sent;
     
-    sigemptyset(&set);
-    sigaddset(&set, SIGUSR2);
-    sigaddset(&set, SIGIO);
-    sigaddset(&set, SIGALRM);
-    sigprocmask(SIG_BLOCK, &set, NULL);
-
     if (parse_host_port(&addr, s->dest_ip) < 0) {
         fprintf(stderr, "wrong dest ip %s\n", s->dest_ip);
         return NULL;
