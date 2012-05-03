cf8685f93299577e988adc717e217cdd6f647f28
sheepx86 <sheepx86@vm>
2012-05-03 00:58:46 -0400

    dummy

--- arch_init.c
+++ arch_init.c
@@ -591,6 +591,7 @@ int ram_load(QEMUFile *f, void *opaque, int version_id, Byte *decomped_buf)
             addr = qemu_get_be64(f);
         else
            addr = buf_get_be64(decomped_buf);
+        DPRINTF("%lx|", addr);
         flags = addr & ~TARGET_PAGE_MASK;
         addr &= TARGET_PAGE_MASK;
 
